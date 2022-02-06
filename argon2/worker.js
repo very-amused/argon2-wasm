992b990516ea54317e98a59d5d6f02007a3e8d29
very-amused <very-amused@pm.me>
2022-02-06 15:46:41 -0500

    Move demo build to pages directory

---
+++ argon2/worker.js
@@ -0,0 +1,211 @@
+class WorkerConnection {
+    constructor(worker) {
+        this.worker = worker;
+        this.worker.addEventListener('message', (evt) => {
+            this.onMessage(evt);
+        }, true);
+    }
+    onMessage(evt) {
+        if (!this.resolve) {
+            return;
+        }
+        this.resolve(evt.data);
+    }
+    postMessage(message, transfer = []) {
+        const p = new Promise((resolve) => {
+            this.resolve = resolve;
+        });
+        this.worker.postMessage(message, transfer);
+        return p;
+    }
+    deinit() {
+        this.worker.removeEventListener('message', (evt) => {
+            this.onMessage(evt);
+        }, true);
+        this.worker.terminate();
+    }
+}
+
+var Argon2;
+(function (Argon2) {
+    Argon2.WorkerConnection = WorkerConnection;
+    (function (Methods) {
+        Methods[Methods["LoadArgon2"] = 0] = "LoadArgon2";
+        Methods[Methods["Hash2i"] = 1] = "Hash2i";
+    })(Argon2.Methods || (Argon2.Methods = {}));
+    (function (ErrorCodes) {
+        ErrorCodes[ErrorCodes["ARGON2_OK"] = 0] = "ARGON2_OK";
+        ErrorCodes[ErrorCodes["ARGON2_OUTPUT_PTR_NULL"] = -1] = "ARGON2_OUTPUT_PTR_NULL";
+        ErrorCodes[ErrorCodes["ARGON2_OUTPUT_TOO_SHORT"] = -2] = "ARGON2_OUTPUT_TOO_SHORT";
+        ErrorCodes[ErrorCodes["ARGON2_OUTPUT_TOO_LONG"] = -3] = "ARGON2_OUTPUT_TOO_LONG";
+        ErrorCodes[ErrorCodes["ARGON2_PWD_TOO_SHORT"] = -4] = "ARGON2_PWD_TOO_SHORT";
+        ErrorCodes[ErrorCodes["ARGON2_PWD_TOO_LONG"] = -5] = "ARGON2_PWD_TOO_LONG";
+        ErrorCodes[ErrorCodes["ARGON2_SALT_TOO_SHORT"] = -6] = "ARGON2_SALT_TOO_SHORT";
+        ErrorCodes[ErrorCodes["ARGON2_SALT_TOO_LONG"] = -7] = "ARGON2_SALT_TOO_LONG";
+        ErrorCodes[ErrorCodes["ARGON2_AD_TOO_SHORT"] = -8] = "ARGON2_AD_TOO_SHORT";
+        ErrorCodes[ErrorCodes["ARGON2_AD_TOO_LONG"] = -9] = "ARGON2_AD_TOO_LONG";
+        ErrorCodes[ErrorCodes["ARGON2_SECRET_TOO_SHORT"] = -10] = "ARGON2_SECRET_TOO_SHORT";
+        ErrorCodes[ErrorCodes["ARGON2_SECRET_TOO_LONG"] = -11] = "ARGON2_SECRET_TOO_LONG";
+        ErrorCodes[ErrorCodes["ARGON2_TIME_TOO_SMALL"] = -12] = "ARGON2_TIME_TOO_SMALL";
+        ErrorCodes[ErrorCodes["ARGON2_TIME_TOO_LARGE"] = -13] = "ARGON2_TIME_TOO_LARGE";
+        ErrorCodes[ErrorCodes["ARGON2_MEMORY_TOO_LITTLE"] = -14] = "ARGON2_MEMORY_TOO_LITTLE";
+        ErrorCodes[ErrorCodes["ARGON2_MEMORY_TOO_MUCH"] = -15] = "ARGON2_MEMORY_TOO_MUCH";
+        ErrorCodes[ErrorCodes["ARGON2_LANES_TOO_FEW"] = -16] = "ARGON2_LANES_TOO_FEW";
+        ErrorCodes[ErrorCodes["ARGON2_LANES_TOO_MANY"] = -17] = "ARGON2_LANES_TOO_MANY";
+        ErrorCodes[ErrorCodes["ARGON2_PWD_PTR_MISMATCH"] = -18] = "ARGON2_PWD_PTR_MISMATCH";
+        ErrorCodes[ErrorCodes["ARGON2_SALT_PTR_MISMATCH"] = -19] = "ARGON2_SALT_PTR_MISMATCH";
+        ErrorCodes[ErrorCodes["ARGON2_SECRET_PTR_MISMATCH"] = -20] = "ARGON2_SECRET_PTR_MISMATCH";
+        ErrorCodes[ErrorCodes["ARGON2_AD_PTR_MISMATCH"] = -21] = "ARGON2_AD_PTR_MISMATCH";
+        ErrorCodes[ErrorCodes["ARGON2_MEMORY_ALLOCATION_ERROR"] = -22] = "ARGON2_MEMORY_ALLOCATION_ERROR";
+        ErrorCodes[ErrorCodes["ARGON2_FREE_MEMORY_CBK_NULL"] = -23] = "ARGON2_FREE_MEMORY_CBK_NULL";
+        ErrorCodes[ErrorCodes["ARGON2_ALLOCATE_MEMORY_CBK_NULL"] = -24] = "ARGON2_ALLOCATE_MEMORY_CBK_NULL";
+        ErrorCodes[ErrorCodes["ARGON2_INCORRECT_PARAMETER"] = -25] = "ARGON2_INCORRECT_PARAMETER";
+        ErrorCodes[ErrorCodes["ARGON2_INCORRECT_TYPE"] = -26] = "ARGON2_INCORRECT_TYPE";
+        ErrorCodes[ErrorCodes["ARGON2_OUT_PTR_MISMATCH"] = -27] = "ARGON2_OUT_PTR_MISMATCH";
+        ErrorCodes[ErrorCodes["ARGON2_THREADS_TOO_FEW"] = -28] = "ARGON2_THREADS_TOO_FEW";
+        ErrorCodes[ErrorCodes["ARGON2_THREADS_TOO_MANY"] = -29] = "ARGON2_THREADS_TOO_MANY";
+        ErrorCodes[ErrorCodes["ARGON2_MISSING_ARGS"] = -30] = "ARGON2_MISSING_ARGS";
+        ErrorCodes[ErrorCodes["ARGON2_ENCODING_FAIL"] = -31] = "ARGON2_ENCODING_FAIL";
+        ErrorCodes[ErrorCodes["ARGON2_DECODING_FAIL"] = -32] = "ARGON2_DECODING_FAIL";
+        ErrorCodes[ErrorCodes["ARGON2_THREAD_FAIL"] = -33] = "ARGON2_THREAD_FAIL";
+        ErrorCodes[ErrorCodes["ARGON2_DECODING_LENGTH_FAIL"] = -34] = "ARGON2_DECODING_LENGTH_FAIL";
+        ErrorCodes[ErrorCodes["ARGON2_VERIFY_MISMATCH"] = -35] = "ARGON2_VERIFY_MISMATCH";
+        ErrorCodes[ErrorCodes["ARGON2WASM_UNKNOWN"] = 1] = "ARGON2WASM_UNKNOWN";
+        ErrorCodes[ErrorCodes["ARGON2WASM_BAD_REQUEST"] = 2] = "ARGON2WASM_BAD_REQUEST";
+        ErrorCodes[ErrorCodes["ARGON2WASM_UNSUPPORTED_BROWSER"] = 3] = "ARGON2WASM_UNSUPPORTED_BROWSER";
+    })(Argon2.ErrorCodes || (Argon2.ErrorCodes = {}));
+})(Argon2 || (Argon2 = {}));
+
+let argon2;
+function getErrorMessage(err) {
+    if (err instanceof Error) {
+        return err.message;
+    }
+    else if (typeof err === 'string') {
+        return err;
+    }
+    else {
+        return 'An unknown error has occured, and a message was unable to be parsed from this error';
+    }
+}
+function postError(err) {
+    if (err in Argon2.ErrorCodes) {
+        postMessage({
+            code: err
+        });
+    }
+    else {
+        postMessage({
+            code: Argon2.ErrorCodes.ARGON2WASM_UNKNOWN,
+            message: getErrorMessage(err)
+        });
+    }
+}
+const postMessage = (message, transfer = []) => {
+    self.postMessage(message, transfer);
+};
+function overwriteSecure(view, passes = 1) {
+    for (let i = 0; i < passes; i++) {
+        for (let j = 0; j < view.length; j++) {
+            view[j] = 0x00;
+        }
+    }
+}
+async function simdSupported(wasmRoot = '.') {
+    const res = await fetch(`${wasmRoot}/simd-test.wasm`);
+    const raw = await res.arrayBuffer();
+    return WebAssembly.validate(raw);
+}
+async function loadArgon2(wasmRoot = '.', simd = false) {
+    if (typeof WebAssembly !== 'object') {
+        throw Argon2.ErrorCodes.ARGON2WASM_UNSUPPORTED_BROWSER;
+    }
+    const opts = {
+        env: {
+            emscripten_notify_memory_growth() {
+            }
+        }
+    };
+    const file = (simd && (await simdSupported(wasmRoot))) ? 'argon2-simd.wasm' : 'argon2.wasm';
+    let source;
+    if (typeof WebAssembly.instantiateStreaming === 'function') {
+        source = await WebAssembly.instantiateStreaming(fetch(`${wasmRoot}/${file}`), opts);
+    }
+    else {
+        const res = await fetch(`${wasmRoot}/${file}`);
+        const raw = await res.arrayBuffer();
+        source = await WebAssembly.instantiate(raw, opts);
+    }
+    return source.instance.exports;
+}
+function hash(options) {
+    const saltLen = options.salt.byteLength;
+    const saltPtr = argon2.malloc(saltLen);
+    let saltView = new Uint8Array(argon2.memory.buffer, saltPtr, saltLen);
+    for (let i = 0; i < saltLen; i++) {
+        saltView[i] = options.salt[i];
+    }
+    const encoded = new TextEncoder().encode(options.password.normalize('NFKC'));
+    const passwordLen = encoded.byteLength;
+    const passwordPtr = argon2.malloc(passwordLen);
+    let passwordView = new Uint8Array(argon2.memory.buffer, passwordPtr, passwordLen);
+    for (let i = 0; i < passwordLen; i++) {
+        passwordView[i] = encoded[i];
+    }
+    overwriteSecure(encoded);
+    const hashLen = options.hashLen;
+    const hashPtr = argon2.malloc(hashLen);
+    const code = argon2.argon2i_hash_raw(options.timeCost, options.memoryCost, 1, passwordPtr, passwordLen, saltPtr, saltLen, hashPtr, hashLen);
+    passwordView = new Uint8Array(argon2.memory.buffer, passwordPtr, passwordLen);
+    overwriteSecure(passwordView);
+    argon2.free(passwordPtr);
+    saltView = new Uint8Array(argon2.memory.buffer, saltPtr, saltLen);
+    overwriteSecure(saltView);
+    argon2.free(saltPtr);
+    overwriteSecure(options.salt);
+    const hash = new Uint8Array(hashLen);
+    const hashView = new Uint8Array(argon2.memory.buffer, hashPtr, hashLen);
+    for (let i = 0; i < hashLen; i++) {
+        hash[i] = hashView[i];
+    }
+    overwriteSecure(hashView);
+    argon2.free(hashPtr);
+    return {
+        code,
+        body: hash
+    };
+}
+onmessage = async function (evt) {
+    if (Array.isArray(evt.data) || typeof evt.data !== 'object') {
+        postMessage({
+            code: Argon2.ErrorCodes.ARGON2WASM_BAD_REQUEST
+        });
+    }
+    const req = evt.data;
+    switch (req.method) {
+        case Argon2.Methods.LoadArgon2:
+            try {
+                const params = req.params;
+                argon2 = await loadArgon2(params.wasmRoot, params.simd);
+            }
+            catch (err) {
+                postError(err);
+                return;
+            }
+            postMessage({
+                code: Argon2.ErrorCodes.ARGON2_OK
+            });
+            break;
+        case Argon2.Methods.Hash2i:
+            const result = hash(req.params);
+            postMessage({
+                code: result.code,
+                body: result.body
+            }, [result.body.buffer]);
+            break;
+        default:
+            postMessage({
+                code: Argon2.ErrorCodes.ARGON2WASM_BAD_REQUEST
+            });
+    }
+};
---
+++ demo.js
@@ -0,0 +1,146 @@
+import {Argon2} from "./runtime/index.js";
+const conn = new Argon2.WorkerConnection(new Worker("/argon2/worker.js"));
+const els = {
+  password: document.querySelector("input#password"),
+  salt: document.querySelector("input#salt"),
+  timeCost: document.querySelector("input#t_cost"),
+  memoryCost: document.querySelector("input#m_cost"),
+  simd: document.querySelector("input#simd_enabled"),
+  showTimer: document.querySelector("input#timer_enabled"),
+  run: document.querySelector("input#submit"),
+  result: document.querySelector("span#result"),
+  timer: document.querySelector("section#timer"),
+  timerValue: document.querySelector("span#timer_value"),
+  form: document.querySelector("form#demoForm")
+};
+const simdCookie = "useSimd";
+const timerCookie = "displayTime";
+let simdEnabled = localStorage.getItem(simdCookie) === "true";
+localStorage.setItem(simdCookie, simdEnabled.toString());
+if (simdEnabled) {
+  els.simd.checked = true;
+}
+if (localStorage.getItem(timerCookie) === "true") {
+  els.timer.className = "show";
+  els.showTimer.checked = true;
+}
+;
+(async function() {
+  const loadMessage = await conn.postMessage({
+    method: Argon2.Methods.LoadArgon2,
+    params: {
+      wasmRoot: ".",
+      simd: simdEnabled
+    }
+  });
+  if (loadMessage.code !== 0) {
+    displayError(loadMessage.code);
+  }
+})();
+document.onclose = () => {
+  conn.deinit();
+};
+els.showTimer.addEventListener("click", () => {
+  els.timer.className = els.showTimer.checked ? "show" : "";
+  localStorage.setItem(timerCookie, els.showTimer.checked.toString());
+});
+els.simd.addEventListener("click", () => {
+  localStorage.setItem(simdCookie, els.simd.checked.toString());
+});
+function writeResult(text) {
+  els.run.disabled = false;
+  els.result.textContent = text;
+}
+function displayError(code) {
+  let errorName = "";
+  for (const name in Argon2.ErrorCodes) {
+    if (Argon2.ErrorCodes[name] === code) {
+      errorName = name;
+      break;
+    }
+  }
+  writeResult(`Error: ${errorName} (code ${code})`);
+}
+els.form.onsubmit = async (evt) => {
+  evt.preventDefault();
+  els.run.disabled = true;
+  els.result.textContent = "";
+  const simd = els.simd.checked;
+  if (simd !== simdEnabled) {
+    const loadMessage = await conn.postMessage({
+      method: Argon2.Methods.LoadArgon2,
+      params: {
+        wasmRoot: ".",
+        simd
+      }
+    });
+    if (loadMessage.code !== 0) {
+      displayError(loadMessage.code);
+      return;
+    }
+    simdEnabled = simd;
+  }
+  let salt;
+  let encodedSalt = els.salt.value;
+  if (encodedSalt.length) {
+    try {
+      const raw = atob(encodedSalt);
+      salt = new Uint8Array(raw.length);
+      for (let i = 0; i < raw.length; i++) {
+        salt[i] = raw.charCodeAt(i);
+      }
+    } catch (err) {
+      const errorMsg = `Failed to decode salt (${err})`;
+      writeResult(`Error: ${errorMsg}`);
+      return;
+    }
+  } else {
+    salt = new Uint8Array(16);
+    crypto.getRandomValues(salt);
+    encodedSalt = btoa(String.fromCharCode.apply(null, Array.from(salt)));
+    els.salt.value = encodedSalt;
+  }
+  const timeCost = parseInt(els.timeCost.value);
+  const memoryCostValue = els.memoryCost.value.toUpperCase();
+  let memoryCost = 0;
+  if (memoryCostValue.endsWith("MB")) {
+    memoryCost = 1024 * parseInt(memoryCostValue);
+  } else if (memoryCostValue.endsWith("GB")) {
+    memoryCost = 1024 * 1024 * parseInt(memoryCostValue);
+  } else {
+    try {
+      memoryCost = parseInt(memoryCostValue);
+      if (isNaN(memoryCost)) {
+        throw "Value entered in form is NaN";
+      }
+    } catch (err) {
+      const errorMsg = `Failed to parse memory cost (${err})`;
+      writeResult(`Error: ${errorMsg}`);
+      return;
+    }
+  }
+  const start = performance.now();
+  const result = await conn.postMessage({
+    method: Argon2.Methods.Hash2i,
+    params: {
+      password: els.password.value,
+      salt,
+      timeCost,
+      memoryCost,
+      hashLen: 32
+    }
+  });
+  let elapsed = performance.now() - start;
+  if (result.code === 0) {
+    const encodedHash = btoa(String.fromCharCode.apply(null, Array.from(result.body)));
+    writeResult(encodedHash);
+  } else {
+    displayError(result.code);
+  }
+  const minutes = Math.floor(elapsed / 6e4).toString();
+  elapsed %= 6e4;
+  const seconds = Math.floor(elapsed / 1e3).toString();
+  elapsed %= 1e3;
+  const ms = elapsed.toString();
+  els.timerValue.textContent = `${minutes}m ${seconds}s ${ms}ms`;
+};
