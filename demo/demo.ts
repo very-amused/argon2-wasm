6aec1f79feb3e3af648aa08725c805258dc8e388
very-amused <very-amused@pm.me>
2025-02-23 21:44:01 -0500

    Fix worker path

--- demo/demo.ts
+++ demo/demo.ts
@@ -1,5 +1,5 @@
 import { Argon2, Base64 as b64 } from '../runtime/index.js'
-const conn = new Argon2.WorkerConnection(new Worker('/argon2/worker.js'))
+const conn = new Argon2.WorkerConnection(new Worker('./argon2/worker.js'))
 
 
 /** @override */
