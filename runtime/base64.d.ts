3642ce06c52b7e6e379672ac6e5af35da48141b7
very-amused <very-amused@pm.me>
2026-03-17 19:38:15 -0400

    Update demo

--- runtime/base64.d.ts
+++ runtime/base64.d.ts
@@ -1,10 +1,16 @@
 export declare namespace Base64 {
+    const StdEncoding = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+    const UrlEncoding = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-_";
+    type Encoding = typeof StdEncoding | typeof UrlEncoding;
     /**
-     * Encode a Uint8Array using Base64 standard encoding
+     * Encode a Uint8Array using Base64.
      */
     function encode(data: Uint8Array): string;
+    function encode(data: Uint8Array, encoding: Encoding): string;
+    function encode(data: Uint8Array, padding: boolean): string;
+    function encode(data: Uint8Array, encoding: Encoding, padding: boolean): string;
     /**
-     * Decode standard encoded Base64 to a Uint8Array.
+     * Decode Base64 to a Uint8Array.
      */
-    function decode(encoded: string): Uint8Array;
+    function decode(encoded: string, encoding?: Encoding): Uint8Array;
 }
